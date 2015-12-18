a8c73544f0d0f17090d122b002a166a04b0f0174
yusukeinyos <aero.n.404.smith@gmail.com>
2015-12-19 02:08:25 +0900

    autocorrelationfunc

---
+++ LPCtest/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LPCtest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //----------------------------------------------------
+        double[] auto_correlation(double[] x)
+        {
+            int N = x.Length;
+            double[] r = new double[N];
+            for (int i = 0; i < N; i++)
+                for (int j = 0; j < N - i; j++)
+                    r[i] += x[j] * x[j + i];
+            return r;
+        }
+        //----------------------------------------------------
+
+    }
+}
